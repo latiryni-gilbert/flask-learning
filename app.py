fc482137bb96bd4bd5b3e244b310f6763e346575
LaTiryni Gilbert <thewealthy1ne@gmail.com>
2026-01-30 15:43:00 -0600

    added more routes

--- app.py
+++ app.py
@@ -4,18 +4,33 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
-    return 'Index Page'
-
 @app.route("/")
 def hello():
     name = request.args.get("name", "Flask")
     return f"Hello, {escape(name)}!"
 
+@app.route('/index')
+def index():
+    return 'Index Page'
+
 @app.route('/expenses')
 def expenses():
     return {'total': 0, 'count': 0}
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
 
 if __name__ == '__main__':
     app.run(debug=True)
