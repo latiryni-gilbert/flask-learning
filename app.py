3e395a66a072fd8f81d096d4aa9c3c58b77655c9
LaTiryni Gilbert <thewealthy1ne@gmail.com>
2026-01-30 15:29:00 -0600

    learned html escaping

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import request
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Hello, World!'
+    name = request.args.get("name", "Flask")
+    return f"Hello, {escape(name)}!"
 
 @app.route('/expenses')
 def expenses():
